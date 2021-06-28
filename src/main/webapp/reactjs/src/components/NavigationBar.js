541062cbb538ec25f90947862a7742f69593311b
mightyjava <jeetparmar949@gmail.com>
2021-06-28 11:29:31 +0530

    Modified navbar-brand onclick logic

--- src/main/webapp/reactjs/src/components/NavigationBar.js
+++ src/main/webapp/reactjs/src/components/NavigationBar.js
@@ -51,7 +51,7 @@ const NavigationBar = (props) => {
 
   return (
     <Navbar bg="dark" variant="dark">
-      <Link to={""} className="navbar-brand">
+      <Link to={props.auth.isLoggedIn ? "home" : ""} className="navbar-brand">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png"
           width="25"
